7d396cd36c595181a4530bf75e7d5ba694d63a15
Elijah Mickelson <elijah.mickelson@ucalgary.ca>
2024-04-29 16:46:46 -0600

    Created dirt terrain
    Former-commit-id: 9c5027879649b84c1fddc981c0886365287d28fb [formerly 7dce4dc1b3536745d86121703a1259d3877bf4b8]
    Former-commit-id: d3c152d865ffd44768a19e54a2c1932230a3ca77

---
+++ Wheat Simulation/Assets/notes.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class notes : MonoBehaviour
+{
+	//Dirt texture:
+	//https://www.deviantart.com/fabooguy/art/Dirt-Ground-Texture-Tileable-2048x2048-441212191
+}
