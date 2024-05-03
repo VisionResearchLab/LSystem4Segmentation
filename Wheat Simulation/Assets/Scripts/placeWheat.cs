be1e4688362fe6cc49b50c776a25f5ba9e0601b0
Elijah Mickelson <elijah.mickelson@ucalgary.ca>
2024-05-02 21:06:42 -0600

    Added a wheat prefab and made basic code to instantiate it
    Former-commit-id: f7b8c60a216144eeb6bb4125c4a51e2ecdfda4b1 [formerly 885fb26d0102d34af007308eec9ba68266e02447] [formerly d9df0c669f4d0b5fed1fb90d3f9ca1008119b8e9 [formerly ca2227553b639952385826036401f4460ab1be4e]] [formerly 5b3f48737f05dc3ddbd12596e78a05ed6159f913 [formerly 758c2699a5d58e514f819f37a8f7e49996adbb3c] [formerly 88f13c32991988a4f178af04ac3ff82f5667d733 [formerly edde94caf8d8c1e4f3c41cd3029f41734686f1e7]]]
    Former-commit-id: 0fb60fef35f5b3b17f553f1feb755c23f1fad830 [formerly c16a0b4427cd757a96e6ab143a928786b671c8fc] [formerly 9b2ecdbc4766a52604efb473c07482a54e845a4f [formerly 3d77eb395985265b5fa45599bdc41f01e8375fef]]
    Former-commit-id: da4900f7b7ecc8cace36e4c29817745ce2c81e3c [formerly 647f1b975792909c7ae7ef88a380b41cdf39cdb9]
    Former-commit-id: f4824ffe76955457b0598c59b754e43fa8c781d3

---
+++ Wheat Simulation/Assets/Scripts/placeWheat.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class placeWheat : MonoBehaviour
+{
+    public GameObject wheat;
+
+    public TMPro.TMP_InputField quantityToPlaceInputField;
+
+    // Range of coordinates on the x axis where wheat can appear
+    private float xMin = 0f;
+    private float xMax = 100f;
+
+    // Range of coordinates on the y axis where wheat can appear
+    private float yMin = 1f;
+    private float yMax = 1.8f;
+
+    // Range of coordinates on the z axis where wheat can appear
+    private float zMin = 0f;
+    private float zMax = 100f;
+
+    // When called, create a new wheat object in a random position that is within the coordinates given above
+    public void InstantiateWheat()
+    {
+        int quantityToPlace = int.Parse(quantityToPlaceInputField.GetComponent<TMPro.TMP_InputField>().text);
+        for (int i = 0; i < quantityToPlace; i++)
+        Instantiate(wheat, new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), Random.Range(zMin, zMax)), Quaternion.Euler(0f, Random.Range(0f, 360f), 0f));
+    }
+}
